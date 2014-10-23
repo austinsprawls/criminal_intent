998fd9173accbaf36ec479e14b5389a3e315620f
Austin Sprawls <austinsprawls@gmail.com>
2014-10-23 10:07:47 -0500

    initial commit

---
+++ src/com/bignerdranch/android/criminalntent/CrimeActivity.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.criminalntent;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
+
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+
+}
